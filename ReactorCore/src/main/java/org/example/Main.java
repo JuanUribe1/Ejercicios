a90b94015fa1bd30633b825337a9921c1fb43a6a
Juan Pablo Uribe <juan.uribe@techandsolve.com>
2022-05-06 14:53:15 -0500

    Reactor core

---
+++ ReactorCore/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> elements = new ArrayList<>();
+
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(elements::add);
+
+
+    }
+
+}
